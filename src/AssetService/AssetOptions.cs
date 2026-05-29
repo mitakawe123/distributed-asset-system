13918e994f425b7d0521a2b88786b5c2d1c2e5b6
dimitar.kyuchukov <dimitar.kyuchukov@egt-digital.com>
2026-05-29 13:57:11 +0300

    update to newer version of postgres

--- src/AssetService/AssetOptions.cs
+++ src/AssetService/AssetOptions.cs
@@ -7,7 +7,7 @@ public sealed class AssetOptions
     public const string SectionName = "Asset";
 
     [Required]
-    public string Id { get; init; } = default!;
+    public string Id { get; init; } = "asset-local";
 
     public int ReportingIntervalSecs { get; init; } = 10;
 }
